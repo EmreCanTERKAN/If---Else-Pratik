71dc632bfbc5013cb019a6b4cd219790c66fe572
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-07-15 17:25:51 +0300

    İç İçe İf Pratik
    Patika Birinci Hafta İç İçe İf Öğreği

---
+++ Project/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+Console.WriteLine("Bir Sayı Giriniz:");
+int sayi = int.Parse(Console.ReadLine());
+
+if (sayi > 10 ){
+    Console.WriteLine("Sayı 10dan Büyüktür.");
+    if (sayi % 2 == 0 ){
+        Console.WriteLine("Sayı Çifttir");
+    }
+    else Console.WriteLine("Sayı Tektir.");
+}
+
+else if (sayi == 10) {
+    Console.WriteLine("Sayı 10'a Eşittir");
+    if (sayi % 2 == 0)
+    {
+        Console.WriteLine("Sayı Çifttir");
+    }
+    else Console.WriteLine("Sayı Tektir.");
+}
+
+else {
+    Console.WriteLine("Sayı 10dan Küçüktür.");
+    if (sayi % 2 == 0)
+    {
+        Console.WriteLine("Sayı Çifttir");
+    }
+    else Console.WriteLine("Sayı Tektir.");
+}
